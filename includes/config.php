eee304a3f70079ef4bb9d4e253d4c31edf093179
ingubingu <connor.kosirog@gmail.com>
2024-11-17 11:42:32 -0500

    Update config.php

--- includes/config.php
+++ includes/config.php
@@ -5,6 +5,6 @@ return [
     'servername' => 'localhost',     // Database server
     'username'   => 'root',          // Your MySQL username
     'password'   => '',              // Your MySQL password (empty string if none)
-    'dbname'     => 'kosi_host_db',  // Your database name
+    'dbname'     => '',  // Your database name
     'charset'    => 'utf8mb4',       // Character set
 ];
